0e2812006f96b3751eee7b9d2ec1857645070c53
robertito13 <roberto.vaccaro@gmail.com>
2018-02-23 18:20:09 -0300

    Cambio de nombre de scripts y actualización de tarea gulp

--- tasks/devel/scripts.js
+++ tasks/devel/scripts.js
@@ -4,13 +4,12 @@ const concat = require('gulp-concat');
 
 module.exports = function(gulp, config) {
 	gulp.task('scripts', function() {
-		return gulp.src( config.source + '/scripts/*.js' )
+		gulp.src( config.source + '/scripts/*.js' )
 		.pipe( plumber() )
 		.pipe( beautify( {
 				'indent_with_tabs': true,
 				'preserve_newlines': true
 		} ) )
-		.pipe( concat( 'anred.js' ) )
 		.pipe( gulp.dest( config.target + '/scripts/' ) )
 	})
 };
--- tasks/prod/scripts.js
+++ tasks/prod/scripts.js
@@ -5,7 +5,6 @@ module.exports = function(gulp, config) {
 	gulp.task('scripts', function() {
 		gulp.src( config.source + '/scripts/*.js' )
 		.pipe( uglify() )
-		.pipe( concat( 'anred.js' ) )
 		.pipe( gulp.dest( config.target + '/scripts/' ) )
 	})
 };
