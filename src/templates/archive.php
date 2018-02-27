7a8d118b614d5a8400d4a0c5351c0a607258eeb8
robertito13 <roberto.vaccaro@gmail.com>
2018-02-27 17:00:08 -0300

    Plantilla del archivo

---
+++ src/templates/archive.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<div class="container archive">
+	<div class="row">
+		<div class="col-md-3 col-sm-12">
+			<?php dynamic_sidebar( 'archive' ); ?>
+		</div>
+		<div class="col-md-9 col-sm-12">
+
+			<h1 class="page-title">
+				<?php
+				if ( is_day() ) :
+						echo 'Archivo del '. get_the_date( 'l j F, Y');
+					elseif ( is_month() ) :
+						echo 'Archivo de ' . get_the_date( 'F Y' );
+					elseif ( is_year() ) :
+						echo 'Archivo año ' . get_the_date( 'Y' );
+					else :
+						_e( 'Archives', 'twentyfourteen' );
+					endif;
+				?>
+			</h1>
+
+			<?php if ( have_posts() ) : ?>
+				<ul>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<li class="result clearfix">
+						<?php
+							if ( has_post_format( 'video' ) ) {
+								echo '<img width="100" height="100" src="' . anred_get_video_thumbnail( get_the_ID(), 'small' ) . '" class="attachment-100x100 size-100x100 wp-post-image" alt="">';
+							} else {
+								the_post_thumbnail( array( 100, 100 ) );
+							}
+						?>
+						<h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+						<div class="date text-muted"><i class="fa fa-calendar"></i> <?php the_date() ?></div>
+						<?php the_excerpt(); ?>
+					</li>
+				<?php endwhile; ?>
+				</ul>
+				<?php anred_paging_nav() ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
--- src/templates/functions.php
+++ src/templates/functions.php
@@ -74,6 +74,15 @@ function anred_theme_setup() {
 	) );
 
 	register_sidebar( array(
+		'name'          => __('Archivo - Barra Lateral', 'anred'),
+		'id'            => 'archive',
+		'before_widget' => '<div class="container-fluid widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="title">',
+		'after_title'   => '</h3>'
+	) );
+
+	register_sidebar( array(
 		'name'          => __('Redes Sociales - Menú', 'anred'),
 		'id'            => 'header-social',
 		'before_widget' => '<section class="widget %1$s %2$s">',
@@ -288,4 +297,46 @@ add_action('future_to_publish', 'auto_featured_image');
 
 function anred_sanitize_checkbox( $input ) {
 	return ( ( isset( $input ) && true == $input ) ? true : false );
+}
+
+function anred_paging_nav() {
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+
+	$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+	$pagenum_link = html_entity_decode( get_pagenum_link() );
+	$query_args   = array();
+	$url_parts    = explode( '?', $pagenum_link );
+
+	if ( isset( $url_parts[1] ) ) {
+		wp_parse_str( $url_parts[1], $query_args );
+	}
+
+	$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+	$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+	$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+	$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+
+	$links = paginate_links( array(
+		'base'     => $pagenum_link,
+		'format'   => $format,
+		'total'    => $GLOBALS['wp_query']->max_num_pages,
+		'current'  => $paged,
+		'mid_size' => 3,
+		'add_args' => array_map( 'urlencode', $query_args ),
+		'prev_text' => 'Anterior',
+		'next_text' => 'Siguiente',
+		'type'      => 'list',
+	) );
+
+	if ( $links ) :
+
+	?>
+	<nav class="navigation paging-navigation" role="navigation">
+		<?php echo $links; ?>
+	</nav>
+	<?php
+	endif;
 }
