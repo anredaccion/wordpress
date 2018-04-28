9a4cf5d59c215fd79cfc8c4e780c07552f06837d
robertito13 <roberto.vaccaro@gmail.com>
2018-04-28 11:04:56 -0300

    Eliminada función para auto destacar imagen

--- src/templates/partials/card.php
+++ src/templates/partials/card.php
@@ -1,50 +1,8 @@
 <?php
 $image = get_the_post_thumbnail_url();
 
-if ( ! $image ) { // No hay imagen destacada seleccionada
-	$images = get_attached_media( 'image' );
-	if ( count( $images ) > 0 ) { // Hay imagenes attached al post
-		foreach ( $images as $attachment ) {
-			$info = @getimagesize( $attachment->guid );
-			if ( !empty( $info ) ) {
-				$image = $attachment->guid;
-				set_post_thumbnail( get_the_ID(), $attachment->ID );
-				break;
-			}
-			
-		}
-	} else { // No hay imagenes attached al post
-		$document = new DOMDocument();
-		@$document->loadHTML( get_the_content() );
-		$xml = simplexml_import_dom( $document );
-		$images = $xml->xpath('//img');
-
-		if ( count( $images ) > 0 ) {
-			foreach ( $images as $item ) {
-				$info = @getimagesize( $item->src );
-				if ( !empty( $info ) ) {
-					$image = esc_url( $item->src );
-	
-					$wp_filetype = wp_check_filetype( basename($image), null );
-			
-					$attachment = array(
-						'post_mime_type' => $wp_filetype['type'],
-						'post_parent'    => get_the_ID(),
-						'post_title'     => preg_replace( '/\.[^.]+$/', '', basename($image) ),
-						'post_content'   => '',
-						'post_status'    => 'inherit'
-					);
-			
-					$attachment_id = wp_insert_attachment( $attachment, $image, get_the_ID() );
-					$attachment_data = wp_generate_attachment_metadata( $attachment_id, $image );
-					wp_update_attachment_metadata( $attachment_id,  $attachment_data );
-					set_post_thumbnail( get_the_ID(), $attachment_id );
-				}
-			}
-		} else { // No hay attachments ni tags img en el contenido
-			$image = get_template_directory_uri() . "/images/logo-nada.png";
-		}
-	}
+if ( ! $image ) {
+	$image = get_template_directory_uri() . "/images/logo-nada.png";
 }
 ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class( array( 'card', 'article-card' ) ); ?>>
