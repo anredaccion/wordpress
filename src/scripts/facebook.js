021b4134c10ae52ccb5457e0408ecebfdc06add7
robertito13 <roberto.vaccaro@gmail.com>
2018-05-28 17:21:59 -0300

    Calculo del width de los videos de facebook para que sean responsive

--- src/scripts/facebook.js
+++ src/scripts/facebook.js
@@ -5,3 +5,7 @@ var js, fjs = d.getElementsByTagName(s)[0];
 	js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+(function($) {
+	$('article .fb-video iframe').attr('src', $('article .fb-video iframe').attr('src').replace(/width=(\d+)/g, 'width=' + $('article').width() ) )
+})( jQuery );
