788be2760cd8e28f281f34a1f0c1eb0938737165
robertito13 <roberto.vaccaro@gmail.com>
2018-07-16 20:09:16 -0300

    Agregada optimización de imágenes

--- tasks/images.js
+++ tasks/images.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 
 module.exports = function( config ) {
 	gulp.task( 'images', gulp.parallel(
 		function screenshot( done ) {
 			gulp.src( config.dirs.source + '/images/screenshot.png' )
+				.pipe(imagemin())
 				.pipe( gulp.dest( config.dirs.build ) )
 				.pipe( gulp.dest( config.dirs.dist ) );
 
@@ -11,6 +13,7 @@ module.exports = function( config ) {
 		},
 		function others( done ) {
 			gulp.src( [ config.dirs.source + '/images/**', '!' + config.dirs.source + '/images/screenshot.png' ] )
+				.pipe(imagemin())
 				.pipe( gulp.dest( config.dirs.build + '/images' ) )
 				.pipe( gulp.dest( config.dirs.dist + '/images' ) );
 
