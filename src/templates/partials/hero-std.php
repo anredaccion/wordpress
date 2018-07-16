ce5b46ef63acccdcba82cbcb460c0bda45576416
robertito13 <roberto.vaccaro@gmail.com>
2018-07-16 17:19:38 -0300

    Agregado soporte a los nuevos tamños de imagen en el carrousel

--- src/templates/partials/hero-std.php
+++ src/templates/partials/hero-std.php
@@ -1,10 +1,13 @@
 <div class="carousel-item <?php echo ($current_post == 0 ? 'active' : ''); ?>">
 	<picture>
-		<source media="(min-width: 1024px)" srcset="<?php the_post_thumbnail_url( 'full' ) ?>">
-		<source media="(min-width: 640px)" srcset="<?php the_post_thumbnail_url( 'large' ) ?>">
-		<source media="(min-width: 300px)" srcset="<?php the_post_thumbnail_url(array(640, 480) ) ?>">
-		<source media="(min-width: 150px)" srcset="<?php the_post_thumbnail_url( 'medium' ) ?>">
-		<img class="d-block w-100" src="<?php the_post_thumbnail_url('thumbnail') ?>">
+		<source media="(min-width: 1101px)" srcset="<?php the_post_thumbnail_url( 'full' ) ?>">
+		<source media="(min-width: 826px)" srcset="<?php the_post_thumbnail_url( 'post-thumbnail' ) ?>">
+		<source media="(min-width: 691px)" srcset="<?php the_post_thumbnail_url( 'article-large' ) ?>">
+		<source media="(min-width: 511px)" srcset="<?php the_post_thumbnail_url( 'article-medium' ) ?>">
+		<source media="(min-width: 384px)" srcset="<?php the_post_thumbnail_url( 'article-small' ) ?>">
+		<source media="(min-width: 316px)" srcset="<?php the_post_thumbnail_url( 'frontpage-2x-big' ) ?>">
+		<source media="(min-width: 226px)" srcset="<?php the_post_thumbnail_url( 'frontpage-2x-medium' ) ?>">
+		<img class="d-block w-100" src="<?php the_post_thumbnail_url( 'frontpage-2x-small' ) ?>">
 	</picture>
 	<div class="carousel-caption d-md-block">
 		<h3><a href="<?php the_permalink() ?>" alt="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
